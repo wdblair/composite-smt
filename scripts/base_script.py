81dc0cbec9380a2f4225fcd5a8aed3ab4a6229e0
Nikka Ghalili <nikka999@bu.edu>
2013-04-16 21:38:00 -0400

    basic base_script

--- scripts/base_script.py
+++ scripts/base_script.py
@@ -1,19 +1,46 @@
 #!/usr/bin/python
 
-import threading, sys, subprocess
+import threading, sys, subprocess, Queue
 
 class Z3(threading.Thread):
-    # def __init__(self):
-    #     pass
-    def run(self, smt):
-        process = subprocess.Popen(['z3', '-smt', smt], stdout=subprocess.PIPE)
+    def __init__(self, smt, q):
+        threading.Thread.__init__(self)
+        self.smt = smt
+        self.q = q
+    def run(self):
+        process = subprocess.Popen(['z3', '-smt', self.smt], stdout=subprocess.PIPE)
         out, err = process.communicate()
-        print out
+        if out.split('\n')[0] == 'unknown':
+            return None        
+        try:
+            self.q.put_nowait(out)
+            print (out, "z3")
+        except Queue.Full:
+            #it loss
+            pass
+            
+class Yices(threading.Thread):
+    def __init__(self, smt, q):
+        threading.Thread.__init__(self)
+        self.smt = smt
+        self.q = q
+    def run(self):
+        process = subprocess.Popen(['yices-smt', self.smt], stdout=subprocess.PIPE)
+        out, err = process.communicate()
+        if out.split('\n')[0] == 'unknown':
+            return None
+        try:
+            self.q.put_nowait(out)
+            print (out, 'yices')
+        except Queue.Full:
+            #it loss
+            pass
 
 
 file = sys.argv[1]
-mutex = threading.Lock()
+q = Queue.Queue(1)
 
-
-z3 = Z3()
-z3.start(file)
+z3 = Z3(file, q)
+yices = Yices(file, q)
+z3.start()
+yices.start()
