44260d7851467bd8d8cb887a8d3d5f3eb38ca1eb
Nikka Ghalili <nikka999@gmail.com>
2013-04-16 20:57:28 -0400

    fix

--- scripts/base_script.py
+++ scripts/base_script.py
@@ -3,10 +3,10 @@
 import threading, sys, subprocess
 
 class Z3(threading.Thread):
-    def __init__(self, smt):
-        self.smt = smt
-    def run(self):
-        process = subprocess.Popen(['z3', '-smt', self.smt], stdout=subprocess.PIPE)
+    # def __init__(self):
+    #     pass
+    def run(self, smt):
+        process = subprocess.Popen(['z3', '-smt', smt], stdout=subprocess.PIPE)
         out, err = process.communicate()
         print out
 
@@ -15,5 +15,5 @@ file = sys.argv[1]
 mutex = threading.Lock()
 
 
-z3 = Z3(file)
-z3.start()
+z3 = Z3()
+z3.start(file)
