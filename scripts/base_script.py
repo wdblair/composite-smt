0c62eaf03f8e24d9f90e53b18711dfb68b0f4088
Nikka Ghalili <nikka999@gmail.com>
2013-04-23 20:47:26 -0400

    updated base script functionality to kill losing process

--- scripts/base_script.py
+++ scripts/base_script.py
@@ -3,13 +3,19 @@
 import threading, sys, subprocess, Queue
 
 class Z3(threading.Thread):
-    def __init__(self, smt, q):
+    def __init__(self, smt, q, killq):
         threading.Thread.__init__(self)
         self.smt = smt
         self.q = q
+        self.killq = killq
     def run(self):
         process = subprocess.Popen(['z3', '-smt', self.smt], stdout=subprocess.PIPE)
+        self.put_nowait(process)
         out, err = process.communicate()
+        killp = process
+        while killp is process:
+            killp = self.killq.get()
+        killp.kill()
         if out.split('\n')[0] == 'unknown':
             return None        
         try:
@@ -20,13 +26,19 @@ class Z3(threading.Thread):
             pass
             
 class Yices(threading.Thread):
-    def __init__(self, smt, q):
+    def __init__(self, smt, q, killq):
         threading.Thread.__init__(self)
         self.smt = smt
         self.q = q
+        self.killq = killq
     def run(self):
         process = subprocess.Popen(['yices-smt', self.smt], stdout=subprocess.PIPE)
+        self.put_nowait(process)
         out, err = process.communicate()
+        killp = process
+        while killp is process:
+            killp = self.killq.get()
+        killp.kill()
         if out.split('\n')[0] == 'unknown':
             return None
         try:
@@ -35,12 +47,11 @@ class Yices(threading.Thread):
         except Queue.Full:
             #it loss
             pass
-
-
+    
 file = sys.argv[1]
 q = Queue.Queue(1)
-
-z3 = Z3(file, q)
-yices = Yices(file, q)
+killq = Queue.Queue(2)
+z3 = Z3(file, q, killq)
+yices = Yices(file, q, killq)
 z3.start()
 yices.start()
